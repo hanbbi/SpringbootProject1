48d65336081c9554d6b1b38a068110f6f3ce8e4e
yangrangrang <gkssk2309@gmail.com>
2022-12-27 16:12:51 +0900

    수정

---
+++ src/test/java/com/project/projectgroup1/service/UserServiceImpTest.java
@@ -0,0 +1,50 @@
+package com.project.projectgroup1.service;
+
+import com.project.projectgroup1.dto.UserDto;
+import com.project.projectgroup1.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceImpTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void listAllTest() {
+        userService.listAllTest();
+    }
+
+    @Test
+    void signup() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void adminModify() {
+    }
+
+    @Test
+    void login() {
+        userService.login("user1","1234");
+    }
+
+    @Test
+    void detail() {
+    }
+
+    @Test
+    void idCheck() {
+    }
+
+    @Test
+    void remove() {
+    }
+}
